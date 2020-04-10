f700000434b1661adfabb30333b75da19e362198
poetry by <2638986+btomaj@users.noreply.github.com>
2020-04-10 13:03:21 +1000

    Removed browser support because console.assert cannot be caught in browsers

--- nodeunit.js
+++ nodeunit.js
@@ -35,137 +35,126 @@
  *
  * @static
  */
-(function () {
-    var _require = typeof require !== "undefined" ? require : function () {
-            return {argv: {slice: function() {return [];}}};
-        },
-        fs = _require("fs"),
-        vm = _require("vm"),
-        path = _require("path"),
-        process = _require("process"),
-        mode = process.argv.slice(2)[0];
+var fs = require("fs"),
+    vm = require("vm"),
+    path = require("path"),
+    process = require("process"),
+    mode = process.argv.slice(2)[0];
 
-    /**
-     * Tests a test suite.
-     *
-     * A test suite is an object composed of an arbitrary number of methods,
-     * each of which is a test case (http://en.wikipedia.org/wiki/XUnit).
-     *
-     * Four special methods can be defined to manage test fixtures:
-     *  - setUpSuite,
-     *  - setUp,
-     *  - tearDown, and
-     *  - tearDownSuite
-     * conforming to JUnit 4 (http://en.wikipedia.org/wiki/JUnit). These four
-     * methods are not test cases, therefore exceptions and/or assertions are
-     * not captured for these methods.
-     *
-     * All other methods are treated as test cases. Properties are permitted but
-     * ignored.
-     *
-     * All methods of the test suite can refer to the test case object using
-     * "this".
-     *
-     * TODO
-     * Run test suites in sequence
-     *
-     * @method evaluateTestSuite
-     *
-     * @param testSuite {Object} The object that defines the test suite.
-     *
-     * @static
-     * @private
-     */
-    async function evaluateTestSuite(testSuite) {
-        "use strict";
+/**
+ * Tests a test suite.
+ *
+ * A test suite is an object composed of an arbitrary number of methods,
+ * each of which is a test case (http://en.wikipedia.org/wiki/XUnit).
+ *
+ * Four special methods can be defined to manage test fixtures:
+ *  - setUpSuite,
+ *  - setUp,
+ *  - tearDown, and
+ *  - tearDownSuite
+ * conforming to JUnit 4 (http://en.wikipedia.org/wiki/JUnit). These four
+ * methods are not test cases, therefore exceptions and/or assertions are
+ * not captured for these methods.
+ *
+ * All other methods are treated as test cases. Properties are permitted but
+ * ignored.
+ *
+ * All methods of the test suite can refer to the test case object using
+ * "this".
+ *
+ * TODO
+ * Run test suites in sequence
+ *
+ * @method evaluateTestSuite
+ *
+ * @param testSuite {Object} The object that defines the test suite.
+ *
+ * @static
+ * @private
+ */
+async function evaluateTestSuite(testSuite) {
+    "use strict";
 
-        var setUpSuite = testSuite.setUpSuite || function () {},
-            setUp = testSuite.setUp || function () {},
-            tearDown = testSuite.tearDown || function () {},
-            tearDownSuite = testSuite.tearDownSuite || function () {},
-            testCase,
+    var setUpSuite = testSuite.setUpSuite || function () {},
+        setUp = testSuite.setUp || function () {},
+        tearDown = testSuite.tearDown || function () {},
+        tearDownSuite = testSuite.tearDownSuite || function () {},
+        testCase,
 
-            testsRun = 0,
-            testsFailed = 0,
-            testResult = "";
+        testsRun = 0,
+        testsFailed = 0,
+        testResult = "";
 
-        delete testSuite.setUpSuite;
-        delete testSuite.setUp;
-        delete testSuite.tearDown;
-        delete testSuite.tearDownSuite;
+    delete testSuite.setUpSuite;
+    delete testSuite.setUp;
+    delete testSuite.tearDown;
+    delete testSuite.tearDownSuite;
 
-        setUpSuite.call(testSuite);
-        for (testCase in testSuite) {
-            if (testSuite.hasOwnProperty(testCase) &&
-                    typeof testSuite[testCase] === "function") {
-                testsRun += 1;
+    setUpSuite.call(testSuite);
+    for (testCase in testSuite) {
+        if (testSuite.hasOwnProperty(testCase) &&
+                typeof testSuite[testCase] === "function") {
+            testsRun += 1;
 
-                setUp.call(testSuite);
+            setUp.call(testSuite);
 
-                try {
-                    await testSuite[testCase]();
+            try {
+                await testSuite[testCase]();
 
-                    testResult = "+ " + testCase;
-                } catch (e) {
-                    testsFailed += 1;
+                testResult = "+ " + testCase;
+            } catch (e) {
+                testsFailed += 1;
 
-                    testResult = "- " + testCase;
-                    if (e.message) {
-                        testResult += ": '" + e.message + "'";
-                    }
-                }
-
-                tearDown.call(testSuite);
-
-                if (mode === "--verbose" || testResult.indexOf("-") === 0) {
-                    console.log(testResult);
+                testResult = "- " + testCase;
+                if (e.message) {
+                    testResult += ": '" + e.message + "'";
                 }
             }
-        }
-        tearDownSuite.call(testSuite);
 
-        console.log(testsRun + " test(s) run; " + testsFailed +
-                " test(s) failed.");
-    }
+            tearDown.call(testSuite);
 
-    /**
-     * Loads the file to be tested into the context of the test file so that its
-     * code can be exercised.
-     *
-     * TODO
-     * Make asynchronous
-     *
-     * @method loadFile
-     *
-     * @param filePath {string} Relative path from the test file to the file to
-     *          load
-     *
-     * @static
-     * @private
-     */
-    function loadFile(filePath) {
-        "use strict";
-
-        var caller = module.parent.filename;
-        caller = caller.slice(0, caller.lastIndexOf("/") + 1);
-
-        global.require = require;
-        global.__filename = __filename;
-        global.__dirname = __dirname;
-
-        vm.runInThisContext(fs.readFileSync(path.join(caller, filePath),
-                "utf-8"));
-    }
-
-    // Public API
-    if (typeof module !== "undefined" && module.exports) {
-        module.exports = {
-            test: evaluateTestSuite,
-            load: loadFile
-        };
-    } else {
-        this.nodeUnit = {
-            test: evaluateTestSuite
+            if (mode === "--verbose" || testResult.indexOf("-") === 0) {
+                console.log(testResult);
+            }
         }
     }
-}).call(this);
+    tearDownSuite.call(testSuite);
+
+    console.log(testsRun + " test(s) run; " + testsFailed +
+            " test(s) failed.");
+}
+
+/**
+ * Loads the file to be tested into the context of the test file so that its
+ * code can be exercised.
+ *
+ * TODO
+ * Make asynchronous
+ *
+ * @method loadFile
+ *
+ * @param filePath {string} Relative path from the test file to the file to
+ *          load
+ *
+ * @static
+ * @private
+ */
+function loadFile(filePath) {
+    "use strict";
+
+    var caller = module.parent.filename;
+    caller = caller.slice(0, caller.lastIndexOf("/") + 1);
+
+    global.require = require;
+    global.__filename = __filename;
+    global.__dirname = __dirname;
+
+    vm.runInThisContext(fs.readFileSync(path.join(caller, filePath),
+            "utf-8"));
+}
+
+// Public API
+module.exports = {
+    test: evaluateTestSuite,
+    load: loadFile
+};
